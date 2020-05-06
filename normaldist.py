aa960652a8e316b6fdb121655eaa2734f469358a
Victor Chen <vchen8761@gmail.com>
2020-05-06 12:33:17 -0400

    pylab normal distribution

--- normaldist.py
+++ normaldist.py
@@ -4,5 +4,13 @@ import random
 dist = []
 for i in range(100000):
     dist.append(random.gauss(0,30))
+    # random.gauss
+    # arg1: mu (mean)
+    # arg2: sigma (std dev)
 pylab.hist(dist, 30)
 pylab.show()
+
+"""
+Normal distributions peak at mean and fall off symmetrically
+
+"""
