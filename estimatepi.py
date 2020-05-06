a79dbc61842782c2523d57d99625f5b888848e4c
Victor Chen <vchen8761@gmail.com>
2020-05-06 15:23:40 -0400

    estimate pi

--- estimatepi.py
+++ estimatepi.py
@@ -2,7 +2,7 @@
 Estimating Pi by simualting Buffon-Laplace Method
 (Monte Carlo Simulation)
 '''
-import random
+import random, numpy
 
 def throwNeedles(numNeedles):
     inCircle = 0
@@ -13,4 +13,25 @@ def throwNeedles(numNeedles):
             inCircle += 1
     return 4*(inCircle/float(numNeedles))
 
-print(throwNeedles(1000000))
+def getEst(numNeedles, numTrials):
+    estimates = []
+    for t in range(numTrials):
+        piGuess = throwNeedles(numNeedles)
+        estimates.append(piGuess)
+    sDev = numpy.std(estimates) 
+    curEst = sum(estimates)/len(estimates)
+    print('Est. = ' + str(curEst) + ', Std. dev. = ' +
+            str(round(sDev, 6)) + ' Needles = ' +
+            str(numNeedles))
+    return (curEst, sDev)
+
+def estPi(precision, numTrials):
+    numNeedles = 1000
+    sDev = precision
+    while sDev >= precision/2:
+        curEst, sDev = getEst(numNeedles, numTrials)
+        numNeedles *= 2
+    return curEst
+
+random.seed(0)
+estPi(0.005, 100)
